5926671db36ba44fa26cad2b3bd16f17ae19f207
Jim Irrer <irrer@umich.edu>
2022-12-08 11:48:44 -0500

    Updated libraries.

--- src/edu/umro/MSOfficeUtil/Excel/ExcelUtil.scala
+++ src/edu/umro/MSOfficeUtil/Excel/ExcelUtil.scala
@@ -16,10 +16,14 @@ package edu.umro.MSOfficeUtil.Excel
  */
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.{Cell, Row, Sheet, Workbook}
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
-import java.io.{File, FileInputStream}
+import java.io.File
+import java.io.FileInputStream
 
 /**
   * Read an Excel spreadsheet.
